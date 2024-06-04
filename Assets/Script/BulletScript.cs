94267ace54e051ffa9705d0e947c0b73dda46fb0
yonokuso <qltenwkd@naver.com>
2024-06-05 04:18:47 +0900

    5-3 Lifetime
    240605 여러 방향으로 Shooting Bullet, Lifetime setting

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -6,10 +6,13 @@ public class BulletScript : MonoBehaviour
 {
 
     public float BulletSpeed = 200f;
+    public Vector3 ShootVector; // 방향값을 가짐
+    public float Lifetime = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * BulletSpeed);
+        GetComponent<Rigidbody>().AddForce(ShootVector * BulletSpeed);
 
         
     }
@@ -17,6 +20,10 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Lifetime -= Time.deltaTime;
+        if (Lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Script/Control5_2Script.cs
+++ Assets/Script/Control5_2Script.cs
@@ -16,7 +16,20 @@ public class Control5_2Script : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(BulletPrefab); // Instantiate - 오브젝트에 액세스
+            // 변수의 변수(프리팹을 실행시키는 명령어화)
+            GameObject BulletObject = Instantiate(BulletPrefab); // Instantiate - 오브젝트에 액세스
+            
+            // 스크립트 가져오기
+            BulletScript bullet = BulletObject.GetComponent<BulletScript>();
+
+            // 새 벡터값을 줌
+            Vector3 ShootVector = new Vector3(
+                Random.Range(-0.01f, 0.01f), 
+                Random.Range(-0.01f, 0.01f), 
+                1
+                );
+            // 벡터 정규화(normalized)
+            bullet.ShootVector = ShootVector.normalized;
         }
     }
 }
